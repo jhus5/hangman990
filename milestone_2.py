6fec5ef4530f073e435ebb20b9d539b82135489f
J Hus <jax5@live.co.uk>
2023-12-10 12:16:47 +0000

    created functions to run checks

--- milestone_2.py
+++ milestone_2.py
@@ -2,13 +2,11 @@ import random
 
 #Create a list containing the names of your 5 favorite fruits.
 word_list = ["apple", "banana", "pear", "grapes", "cherry"]
-print(word_list)
 
-word = random.choice(word_list)
+#random method to select from list
+random_word_from_word_list = random.choice(word_list)
 
-print(word)
-
-##user input
+#user input
 guess = input(str("Enter a single letter :"))
 #checks
 if len(guess) == 1 and guess.isalpha():
