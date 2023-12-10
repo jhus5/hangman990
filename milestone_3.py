5f1f42c97b1d1c39a46a0cbe6f18cc68514d41ec
J Hus <jax5@live.co.uk>
2023-12-10 12:29:21 +0000

    refactiored milstone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -1,27 +1,27 @@
 import random
 
 #Create a list containing the names of your 5 favorite fruits.
-word_list = ["apple", "banana", "pear", "grapes", "cherry"]
+word_list_of_fruits = ["apple", "banana", "pear", "grapes", "cherry"]
 
 #random method to select from list
-random_word_from_word_list = random.choice(word_list)
+random_fruit_from_list = random.choice(word_list_of_fruits)
 
 #get and check user input
 def ask_for_input():
     while True:
         #user input
-        guess = input(str("Enter a single letter :"))
+        user_guesses_a_letter = input(str("Enter a single letter :"))
         #checks
-        if len(guess) == 1 and guess.isalpha():
-            return guess
+        if len(user_guesses_a_letter) == 1 and user_guesses_a_letter.isalpha():
+            return user_guesses_a_letter
         else:
             print("Invalid letter. Please, enter a single alphabetical character.")
 
 #check if letter is in random word from word list
-def check_guess(guess): 
-    guess = guess.lower()
-    if guess in random_word_from_word_list:
-        print(f"Good guess! {guess} is in the word.")
+def check_guess(user_guesses_a_letter): 
+    letter_in_lowercase = user_guesses_a_letter.lower()
+    if letter_in_lowercase in random_fruit_from_list:
+        print(f"Good guess! {user_guesses_a_letter} is in the word.")
     else: 
         print("Sorry, {guess} is not in the word. Try again.")
           
